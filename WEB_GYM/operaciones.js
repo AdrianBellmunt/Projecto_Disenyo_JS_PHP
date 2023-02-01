e314b5af7982bdf81c3531068a43a41abf70ac6b
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2023-02-01 11:40:07 +0100

    calculadoras hechas

--- WEB_GYM/operaciones.js
+++ WEB_GYM/operaciones.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
     $("#botoncalcular").click(function(event) {
-        $("#caja").load('calculadoraIMC.html');
+        $("#caja").load('CalculadoraMassa.html');
     });
 });
+
+$(document).ready(function() {
+    $("#botoncalcular2").click(function(event) {
+        $("#caja2").load('calculadoraIMC.html');
+    });
+});
+
+$(document).ready(function() {
+    $("#botoncalcular3").click(function(event) {
+        $("#caja3").load('CalculadoraMantener.html');
+    });
+});
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+      $("#contenedor2").hide();
+    });
+    $("#show").click(function(){
+        $("#contenedor2").show();
+      });
+  });
+
 
 
 function calcularIMC() {
@@ -12,4 +34,22 @@ function calcularIMC() {
 
     var imc = peso / (altura*altura);
     alert("Su IMC es: "+ imc.toFixed(2));   
+}
+
+function AumentarPeso() {
+
+    var pesoA = document.getElementById("pesoA").value;
+
+    var aumentarpeso = (pesoA * 22) * 1.5 + 300;
+
+    alert("Calorias necessarias para aumentar masa muscular: "+ aumentarpeso +" kcal");
+    
+}
+
+function MantenerCalorias() {
+
+    var pesoM = document.getElementById("pesoM").value;
+   
+    var mantenerpeso = (pesoM * 22);
+    alert("Calorias necessarias para mantener peso: "+ mantenerpeso +" kcal");
 }
